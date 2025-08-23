a9f7347673f7e2e66e9dd46c3ef0e77ed8336bfb
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-23 14:47:10 -0400

    Database/ Server Explorer Connection string
    Project/Data/AppDbContext
    Startup.cs

---
+++ SchoolApp.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolApp.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
