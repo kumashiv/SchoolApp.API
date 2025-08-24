b5413f42f7c277b451865af284ab1289b4b21357
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-24 15:25:29 -0400

    2.4 Add the authentication controller

---
+++ SchoolApp.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SchoolApp.API.Data;
+using SchoolApp.API.Data.Models;
+
+namespace SchoolApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            AppDbContext context,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+            _configuration = configuration;
+        }
+    }
+}
