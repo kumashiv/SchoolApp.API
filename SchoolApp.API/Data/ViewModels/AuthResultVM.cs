0b777f5f765a45242489ce1ad099fa53e6d1a836
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-24 18:09:35 -0400

    2.9 Generating and storing refresh tokens
    GenerateJWTTokenAsync – Modify
    
    AuthResultVM.cs  - Modify
    - Add property RefreshToken

--- SchoolApp.API/Data/ViewModels/AuthResultVM.cs
+++ SchoolApp.API/Data/ViewModels/AuthResultVM.cs
@@ -5,7 +5,7 @@ namespace SchoolApp.API.Data.ViewModels
     public class AuthResultVM
     {
         public string Token { get; set; }
-
+        public string RefreshToken { get; set; }
         public DateTime ExpiresAt { get; set; }
     }
 }
